fa6bf6907f33efeae8807b2ae1ad07e1900ee7e0
John Leonardo <jdleonardo5@icloud.com>
2017-01-29 16:41:08 -0800

    done with ch1/1

--- Assignments/CH1-1.java
+++ Assignments/CH1-1.java
@@ -7,12 +7,32 @@ Once these items have been entered, the program should calculate and display the
 2. the total of the sale
  */
 
+import java.util.Scanner;
 
 public class ch1_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//scanner init
+		Scanner sc = new Scanner(System.in);
+		
+		//variables
+		double retailPrice, taxRate, salesTax, totalSale;
+		
+		//get the retail price
+		System.out.println("Enter the retail price for the item: ");
+		retailPrice = sc.nextDouble();
+		
+		//get tax rate
+		System.out.println("Enter the sales tax rate: ");
+		taxRate = sc.nextDouble();
+		
+		//calculate salesTax and total price
+		salesTax = (taxRate * retailPrice);
+		totalSale = (salesTax + retailPrice);
+		
+		//display total to user
+		System.out.printf("The sales tax was $%s and the total cost of the item after tax is $%s", salesTax, totalSale);
+		
 	}
 
 }
