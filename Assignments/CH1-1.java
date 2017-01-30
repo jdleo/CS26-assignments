537d202d7353c572a7b001d8272c06d8229ad491
John Leonardo <jdleonardo5@icloud.com>
2017-01-29 16:29:29 -0800

    Update CH1-1.java

--- Assignments/CH1-1.java
+++ Assignments/CH1-1.java
@@ -1,1 +1,18 @@
-//nothing here yet just init
+/*
+a program that calculates the total of a retail sale should ask the user for the following:
+1. the retail price of the item being purchased
+2. the sales tax rate
+Once these items have been entered, the program should calculate and display the following:
+1. the sales tax for the purchase
+2. the total of the sale
+ */
+
+
+public class ch1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
