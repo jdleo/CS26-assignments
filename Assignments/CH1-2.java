e6ae43fe5d106926c8aa535c0a4bb62a7685172e
John Leonardo <jdleonardo5@icloud.com>
2017-01-29 16:42:32 -0800

    Update CH1-2.java

--- Assignments/CH1-2.java
+++ Assignments/CH1-2.java
@@ -1,3 +1,18 @@
-/*
-init
-*/
+/**
+A program that calculates a customer's bank account balance
+A program that calculates the current balance in a savings account must ask the user for the followings:
+1. the starting balance this month
+2. the total amount of deposits made this month
+3. the total amount of withdrawal made this month
+4. the monthly interest rate
+Once the program calculates the current balance, it will be displayed on the screen
+ */
+
+public class ch1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
