49004dd5e2be8dc2f0bba731f2d4862966d7652a
John Leonardo <jdleonardo5@icloud.com>
2017-01-30 16:37:26 -0800

    Create Case Study 3.java

---
+++ Case Studies/Case Study 3.java
@@ -0,0 +1,10 @@
+/*
+The National Commerce Bank has hired you as a contract programmer. Your first assignment
+is to write a class that will be used by the bank’s automated teller machines (ATMs)
+to validate a customer’s personal identification number (PIN). Your class, which will be
+named PinTester, will be incorporated into a larger program that asks the customer to
+input his or her PIN on the ATM’s numeric keypad. (PINs are multi-digit numbers. The
+program stores each digit in an element of an int array.) The program also retrieves a
+copy of the customer’s actual PIN from a database, as an int array. If these arrays match,
+then the customer’s identity is validated.
+*/
