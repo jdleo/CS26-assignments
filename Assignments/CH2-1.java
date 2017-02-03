dea6a0623c4284aaa4e46e6fa845a8502708e3f8
John Leonardo <jdleonardo5@icloud.com>
2017-02-03 09:59:33 -0800

    Update CH2-1.java

--- Assignments/CH2-1.java
+++ Assignments/CH2-1.java
@@ -1,1 +1,9 @@
-/
+/**
+Design a program that will ask the user to enter the amount of a purchase.
+The program should then compute the state sales tax and the county sales tax. 
+Assume the state sales tax is 4 percent of the purchase amount, the county sales
+tax is 2 percent of the purchase amount. The program should display the amount of
+the purchase, the state sales tax, the county sales tax, the total sales tax,
+and the total of the sale (which is the sum of the amount of purchase plus the total sales tax)
+Hint: Use 0.04 for 4 percent, 0.02 for 2 percent in the calculation.
+*/
