4a920cd837ed72d29971de12840dc7979176ad7a
John Leonardo <jdleonardo5@icloud.com>
2017-02-03 10:12:24 -0800

    assignment

--- Assignments/CH2-2.java
+++ Assignments/CH2-2.java
@@ -1,1 +1,9 @@
-
+/**
+Question 2: A soft drink company recently surveyed 12467 of its customers
+and found that approximately 14 percent of those surveyed purchase one or
+more energy drink per week. Of those customer who purchase energy drinks,
+approximately 64 percent of them prefer citrus-flavored energy drinks.
+Write a program that displays the following:
+-the approximate number of customers who purchase one or more energy drinks per week
+-the approximate number of customers who prefer citrus-flavor energy drinks
+*/
