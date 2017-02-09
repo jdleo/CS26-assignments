1420f17bee40ae283a283357c59855808de1af8c
John Leonardo <jdleonardo5@icloud.com>
2017-02-09 15:03:13 -0800

    done

--- labs/ch3lab.java
+++ labs/ch3lab.java
@@ -1,3 +1,4 @@
+package labs;
 /**
  * PRICING PLANS:
  * Package A: 9.95$/month for 10 hours. +hours are 2/per
@@ -24,7 +25,7 @@ public class ch3lab {
 		if(paymentPlan.toLowerCase().equals("a")) {
 			//first check if over 10 hours.
 			if(hoursUsed>10.0) {
-				totalCharge = (9.95 + (hoursUsed * 2));
+				totalCharge = (9.95 + ((hoursUsed-10) * 2));
 				JOptionPane.showMessageDialog(null, String.format("Total charge: $%.2f", totalCharge));
 			} else {
 				totalCharge = 9.95;
@@ -33,20 +34,42 @@ public class ch3lab {
 
 			//now lets check how much money they would save by switching to either B or C
 			double if_B = (13.95 +(hoursUsed * 1));
+			double B_savings;
 			double if_C = 29.99;
 
-			double B_savings = (totalCharge - if_B);
+			//check if we used under 20 hours to calculate B savings
+			if (hoursUsed < 20) {
+				B_savings = (totalCharge - if_B);
+			} else {
+				B_savings = (totalCharge - (if_B - 20));
+			}
+			
 			double C_savings = (totalCharge - if_C);
 
-
-			String msg = String.format("Savings with Package A: $%.2f\nSavings with Package B: $%.2f", B_savings, C_savings);
-			JOptionPane.showMessageDialog(null, msg);
+			//check if savings are negative
+			if ((B_savings < 0) && (C_savings < 0)) {
+				//NO SAVINGS
+				JOptionPane.showMessageDialog(null, "You have the most optimal plan. There were no savings with the other plans.");
+			} else if(B_savings < 0) {
+				//just b savings are negative, so just show user C savings
+				String msg = String.format("Savings with Package C: $%.2f", C_savings);
+				JOptionPane.showMessageDialog(null, msg);
+			} else if(C_savings < 0) {
+				//just c savings are negative, so just show user B savings
+				String msg = String.format("Savings with Package B: $%.2f", B_savings);
+				JOptionPane.showMessageDialog(null, msg);
+			} else {
+				//no negatives, display normal message
+				String msg = String.format("Savings with Package B: $%.2f\nSavings with Package C: $%.2f", B_savings, C_savings);
+				JOptionPane.showMessageDialog(null, msg);
+			}
+			
 
 
 		} else if(paymentPlan.toLowerCase().equals("b")) {
 			//first check if over 20 hours
 			if(hoursUsed>20.0) {
-				totalCharge = (13.95 +(hoursUsed * 1));
+				totalCharge = (13.95 +((hoursUsed-20) * 1));
 				JOptionPane.showMessageDialog(null, String.format("Total charge: $%.2f", totalCharge));
 			} else {
 				totalCharge = 13.95;
@@ -56,8 +79,13 @@ public class ch3lab {
 			double if_C = 29.99;
 			double C_savings = (totalCharge - if_C);
 
-			String msg = String.format("Savings with Package C: %.2f", if_C);
-			JOptionPane.showMessageDialog(null, msg);
+			if (C_savings >= 0) {
+				String msg = String.format("Savings with Package C: $%.2f", C_savings);
+				JOptionPane.showMessageDialog(null, msg);
+			} else {
+				JOptionPane.showMessageDialog(null, "You have the most optimal payment plan. \nThere were no savings if you were to switch to Package C.");
+			}
+			
 
 		} else if(paymentPlan.toLowerCase().equals("c")) {
 			JOptionPane.showMessageDialog(null, "Total charge: $29.99");
@@ -65,10 +93,6 @@ public class ch3lab {
 			JOptionPane.showMessageDialog(null, "INVALID OPTION!");
 			System.exit(0);
 		}
-
-
-
-
 
 	}
 }
