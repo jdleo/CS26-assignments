d96021a01a8e16b7372b963cb20fcc27b839b53c
John Leonardo <jdleonardo5@icloud.com>
2017-02-10 11:43:41 -0800

    Create CH3.java

---
+++ Assignments/CH3.java
@@ -0,0 +1,18 @@
+/**
+ * @author John Leonardo
+ * 2 pounds or less - 1.10
+ * over 2 pounds , less than 6 pounds - 2.20
+ * over 6 pounds , but less than 10 pounds - 3.70
+ * 10 or more - 4.80
+ * (per 500 miles)
+ */
+
+
+public class ch3_assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
