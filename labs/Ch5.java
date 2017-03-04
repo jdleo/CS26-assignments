848077bc15bbc1b186648827c8273b8856c802c7
John Leonardo <jdleonardo5@icloud.com>
2017-03-03 16:35:54 -0800

    random()

--- labs/Ch5.java
+++ labs/Ch5.java
@@ -1,9 +1,19 @@
+/** @author John Leonardo */
+
+import java.util.*;
 
 public class Ch5_Lab {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
+	
+	/** This method generates a random integer from 1-10
+	    @return random integer in the range of 1 to 10
+	 */
+	public static int random() {
+		Random rand = new Random();
+		int n = rand.nextInt(10) + 1;
+		return n;
+	}
 }
