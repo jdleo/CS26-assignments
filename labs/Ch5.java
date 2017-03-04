b388d83f77ce90d1f8cbd250cf4a82f4e0a737f2
John Leonardo <jdleonardo5@icloud.com>
2017-03-03 16:56:11 -0800

    now returning total rainfall
    todo: calculate average monthly rainfall, display all of this, and save it to a txt file named rainfall.txt

--- labs/Ch5.java
+++ labs/Ch5.java
@@ -1,11 +1,21 @@
 /** @author John Leonardo */
 
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class Ch5_Lab {
 
 	public static void main(String[] args) {
+		final int months = 12; //constant for months
+		int years; //int variable
+		double rainfall; //double variable
 		
+		//generate random amount of years and display to user
+		years = random();
+		JOptionPane.showMessageDialog(null, years + " years.");
+		
+		//get rainfall with our method
+		rainfall = getRainfall(years, months);
 	}
 	
 	/** This method generates a random integer from 1-10
@@ -16,4 +26,35 @@ public class Ch5_Lab {
 		int n = rand.nextInt(10) + 1;
 		return n;
 	}
+	
+	/** This method loops through the given number of years & months
+    	@return total rainfall
+     */
+	public static double getRainfall(int years, int months) {
+		double totalRainfall = 0; //temporary variable for total rainfall
+		
+		//OUTER LOOP, loops through years
+		for ( int i = 1; i <= years ; i++ ) {
+			
+			//INNER LOOP, loops through months
+			for ( int j = 1; j <= months ; j++ ) {
+				double rainfallThisMonth;
+				
+				//format our message to give the user, and get rainfall
+				String msg = String.format("Enter the rainfall in inches "+
+											"for year %s, month %s", i, j);
+				//validate if not negative
+				do {
+					String rainfallInput = JOptionPane.showInputDialog(msg);
+					rainfallThisMonth = Double.parseDouble(rainfallInput);
+				} while (rainfallThisMonth < 0);
+				
+				totalRainfall += rainfallThisMonth;
+				
+			}
+		}
+		return totalRainfall; //return total rainfall
+	}
+	
+	
 }
