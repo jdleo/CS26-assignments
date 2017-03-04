ed5bea649ab6a6a184a7294bf4f6d9db62ace492
John Leonardo <jdleonardo5@icloud.com>
2017-03-03 17:11:58 -0800

    done

--- labs/Ch5.java
+++ labs/Ch5.java
@@ -1,11 +1,14 @@
 /** @author John Leonardo */
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.util.*;
 import javax.swing.JOptionPane;
 
 public class Ch5_Lab {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		final int months = 12; //constant for months
 		int years; //int variable
 		double rainfall; //double variable
@@ -16,6 +19,9 @@ public class Ch5_Lab {
 		
 		//get rainfall with our method
 		rainfall = getRainfall(years, months);
+		
+		//calculate, display, and write
+		calculateDisplayWrite(years, months, rainfall);
 	}
 	
 	/** This method generates a random integer from 1-10
@@ -29,6 +35,8 @@ public class Ch5_Lab {
 	
 	/** This method loops through the given number of years & months
     	@return total rainfall
+    	@param years as int
+    	@param months as int
      */
 	public static double getRainfall(int years, int months) {
 		double totalRainfall = 0; //temporary variable for total rainfall
@@ -56,5 +64,31 @@ public class Ch5_Lab {
 		return totalRainfall; //return total rainfall
 	}
 	
-	
+	/** This method calculates everything, displays it, and writes it to txt file
+		@param years as int
+		@param months as int
+		@param total rainfall as double
+	        @throws FileNotFoundException 
+	 */
+	public static void calculateDisplayWrite(int years, int months, double rainfall) 
+			throws FileNotFoundException {
+		//make calculations
+		int totalMonths = ( years * months );
+		double averageRainfall = ( rainfall / totalMonths );
+		
+		//format our string
+		String resultMsg = String.format("Total number of months: %s \n"+
+										 "Total rainfall: %.2f inches \n"+
+										 "Average rainfall: %.2f inches",
+										  totalMonths, rainfall, averageRainfall);
+		
+		//display message
+		JOptionPane.showMessageDialog(null, resultMsg);
+		
+		//write result msg to txt file
+		PrintWriter writer = new PrintWriter("rainfall.txt");
+		writer.println(resultMsg);
+		writer.close();
+		
+	}
 }
