b21ea8c2d2efc29803dff2b1dac08ac88f0a9ace
John Leonardo <jdleonardo5@icloud.com>
2017-03-06 16:50:45 -0800

    random() method

--- Assignments/RockPaperScissors.java
+++ Assignments/RockPaperScissors.java
@@ -1,10 +1,21 @@
 /** @author John Leonardo */
 
+import java.util.*; //for random
+
 public class RockPaperScissors {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
+	
+	/**
+	 * This method generates a random integer between 1-3
+	 * @return returns the random int
+	 */
+	public static int random() {
+		//generate random int between 1-3 and return it
+		Random rand = new Random();
+		return rand.nextInt(3) + 1;
+	}
 }
