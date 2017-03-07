fe7bffbe6ecc4efc66496717861a33959f947afa
John Leonardo <jdleonardo5@icloud.com>
2017-03-06 16:59:38 -0800

    picker() method

--- Assignments/RockPaperScissors.java
+++ Assignments/RockPaperScissors.java
@@ -1,6 +1,7 @@
 /** @author John Leonardo */
 
 import java.util.*; //for random
+import javax.swing.*; //for JOptionPane
 
 public class RockPaperScissors {
 
@@ -17,5 +18,27 @@ public class RockPaperScissors {
 		//generate random int between 1-3 and return it
 		Random rand = new Random();
 		return rand.nextInt(3) + 1;
+	}
+	
+	/**
+	 * This method allows the user to pick Rock, Paper, or Scissors
+	 * @return returns the equivalent int for the choice
+	 */
+	public static int picker() {
+		String choice = JOptionPane.showInputDialog("Rock, Paper, or Scissors?"+
+													"\n You can use r,p, or s");
+		char choiceChar = choice.toLowerCase().charAt(0);
+		switch (choiceChar) {
+		case 'r':
+			return 1;
+		case 'p':
+			return 2;
+		case 's':
+			return 3;
+		default:
+			JOptionPane.showMessageDialog(null,"Invalid choice");
+			return -1;
+		}
+		
 	}
 }
