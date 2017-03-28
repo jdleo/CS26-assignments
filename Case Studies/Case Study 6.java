8d33bc16cd5d4e0a8f8542533b8624516b8fd405
John Leonardo <jdleonardo5@icloud.com>
2017-03-27 17:18:44 -0700

    Update Case Study 6.java

--- Case Studies/Case Study 6.java
+++ Case Studies/Case Study 6.java
@@ -1,8 +1,11 @@
-
+/**
+ * Software for validating serial numbers.
+ * A valid serial number is LLLLL-DDDD-LLLL where L are alpha chars and D are numeric chars
+ * @author jdleo
+ */
 public class CaseStudy6 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
