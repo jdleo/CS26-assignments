30127e714b1937faca6ef4a9de3c5c22369a424e
John Leonardo <jdleonardo5@icloud.com>
2017-03-27 18:19:51 -0700

    done, just want to add something that shows the user more information about their invalid SN

--- Case Studies/Case Study 6.java
+++ Case Studies/Case Study 6.java
@@ -20,10 +20,10 @@ public class CaseStudy6 {
 		
 		//display appropriate message to user
 		if (valid) {
-			String m = String.format("%s \n -----VALID-----", serialNumber);
+			String m = String.format("%s \n VALID", serialNumber);
 			JOptionPane.showMessageDialog(null, m);
 		} else {
-			String m = String.format("%s \n -----INVALID-----", serialNumber);
+			String m = String.format("%s \n INVALID", serialNumber);
 			JOptionPane.showMessageDialog(null, m);
 		}
 	}
@@ -35,7 +35,7 @@ public class CaseStudy6 {
 	public static String getSerial() {
 		String sn = JOptionPane.showInputDialog("Enter the serial number you want to validate.\n"+
 												"Must be in LLLLL-DDDD-LLLL format.");
-		return sn;
+		return sn.toUpperCase();
 	}
 	
 	/**
@@ -44,7 +44,71 @@ public class CaseStudy6 {
 	 * @return returns true/false whether it's valid/invalid
 	 */
 	public static boolean validate(String sn) {
-		return false;
+		//first and foremost check if the sn is 15 characters
+		if (sn.length() == 15) {
+			//next, check if it at least contains a hyphen
+			if (sn.contains("-")) {
+				//split the string up divided by hyphens
+				String[] sets = sn.split("-");
+				//next, check if there are three "sets" in the serial number
+				if (sets.length == 3) {
+					//next, check the format alphas-numerics-alphas (wrote a method for this)
+					if (checkFormat(sets)) {
+						return true;
+					} else {
+						return false;
+					}
+				} else {
+					return false;
+				}
+			} else {
+				return false;
+			}
+			
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * This method is to check if the code in LLLLL-DDDD-LLLL format is in a correct format.
+	 * Meaning, the L's MUST be alphas, and the D's MUST be numerics.
+	 * @param sets
+	 * @return Returns true/false
+	 */
+	public static boolean checkFormat(String[] sets) {
+		int score1 = 0; //first set must have a validity score of 5
+		int score2 = 0; //must be 4
+		int score3 = 0; //must be 4
+		
+		String set1 = sets[0];
+		String set2 = sets[1];
+		String set3 = sets[2];
+		
+		for (int i = 0; i <= set1.length()-1; i++) {
+			if ( Character.isLetter(set1.charAt(i)) ) {
+				score1 += 1;
+			}
+		}
+		
+		for (int i = 0; i <= set2.length()-1; i++) {
+			if ( Character.isDigit(set2.charAt(i)) ) {
+				score2 += 1;
+			}
+		}
+
+		for (int i = 0; i <= set3.length()-1; i++) {
+			if ( Character.isLetter(set3.charAt(i)) ) {
+				score3 += 1;
+			}
+		}
+		
+		if (score1 == 5 && score2 == 4 && score3 == 4) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 
 }
