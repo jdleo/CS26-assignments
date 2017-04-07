0666328fcbf7f95844c6e6ae40c73fa646287941
John Leonardo <jdleonardo5@icloud.com>
2017-04-06 21:32:20 -0700

    dice class imported

--- Assignments/Chapter 6/Dice.java
+++ Assignments/Chapter 6/Dice.java
@@ -1,4 +1,50 @@
+import java.util.Random;
 
+/**
+ * The Dice class simulates a six-sided die
+ */
 public class Dice {
+	
+	private int sides; //number of sides
+	private int value; //the die's value
+	
+	/**
+	 * The constructor performs an initial roll of the die.
+	 * @param numSides
+	 */
+	public Dice(int numSides) {
+		sides = numSides;
+		roll();
+	}
+	
+	/**
+	 * This method simulates rolling a dice
+	 */
+	public void roll() {
+		//create Random object
+		Random r = new Random();
+		
+		//get random value
+		value = r.nextInt(sides) + 1;
+	}
 
+	
+	/**
+	 * get sides
+	 * @return sides
+	 */
+	public int getSides() {
+		return sides;
+	}
+
+	/**
+	 * get value
+	 * @return
+	 */
+	public int getValue() {
+		return value;
+	}
+	
+	
+	
 }
