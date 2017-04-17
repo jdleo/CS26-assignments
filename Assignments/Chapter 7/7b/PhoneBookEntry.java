9de950216e51f7d514f0374c83b569187dec9499
John Leonardo <jdleonardo5@icloud.com>
2017-04-17 11:46:59 -0700

    done with phonebookentry

--- Assignments/Chapter 7/7b/PhoneBookEntry.java
+++ Assignments/Chapter 7/7b/PhoneBookEntry.java
@@ -1,4 +1,43 @@
+import javax.swing.JOptionPane;
 
 public class PhoneBookEntry {
+	private String name;
+	private int number;
+	
+	/**
+	 * Constructor for PhoneBookEntry object
+	 * @param name
+	 * @param number
+	 */
+	public PhoneBookEntry(String name, int number) {
+		this.name = name;
+		this.number = number;
+	}
 
+	
+	// --- BEGIN GETTERS/SETTERS --- //
+	public String getName() {
+		return name;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void setNumber(int number) {
+		this.number = number;
+	}
+	// --- END GETTERS/SETTERS --- //
+	
+	/**
+	 * Method that displays the current fields in a JOptionPane window
+	 */
+	public void display() {
+		String msg = String.format("Name: %s\n Number: %s", this.name, this.number);
+		JOptionPane.showMessageDialog(null, msg);
+	}	
 }
