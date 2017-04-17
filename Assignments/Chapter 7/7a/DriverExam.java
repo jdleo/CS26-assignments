d031c0f7100d3a53409a6218b1f92c8b19177250
John Leonardo <jdleonardo5@icloud.com>
2017-04-16 19:48:03 -0700

    passed() and totalCorrect() completed
    todo: totalIncorrect() and questionsMissed()

--- Assignments/Chapter 7/7a/DriverExam.java
+++ Assignments/Chapter 7/7a/DriverExam.java
@@ -24,7 +24,42 @@ public class DriverExam {
 	}
 	// --- END GETTERS --- //
 	
+	/**
+	 * Method for determining whether the student passed or failed
+	 * @param studentAnswers
+	 * @param answerKey
+	 * @return true / false depending on passing
+	 */
+	public static boolean passed(String[] studentAnswers, String[] answerKey) {
+		int totalCorrect = totalCorrect(studentAnswers, answerKey);
+		// student must correctly answer 15 out of 20, to pass the exam
+		if (totalCorrect >= 15) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static int totalCorrect(String[] studentAnswers, String[] answerKey) {
+		int totalCorrect = 0;
+		
+		// for-loop to loop through the array
+		for (int index = 0; index < studentAnswers.length; index++) {
+			if (studentAnswers[index].equals(answerKey[index])) {
+				totalCorrect += 1;
+			}
+		}
+		
+		return totalCorrect;
+	}
+	
+	public static int totalIncorrect(String[] studentAnswers, String[] answerKey) {
+		
+	}
+	
+	public static int[] questionsMissed(String[] studentAnswers, String[] answerKey) {
+		
+	}
 	
 	
 }
-
