789a551bb413626b9299d8b88e6298c8f1d2f354
John Leonardo <jdleonardo5@icloud.com>
2017-04-23 12:53:19 -0700

    Delete PhoneBookEntry.java

--- Assignments/Chapter 7/7b/PhoneBookEntry.java
+++
@@ -1,6 +0,0 @@
-import javax.swing.JOptionPane;public class PhoneBookEntry{private String name;private long number;public PhoneBookEntry(String name,long number){this.name=name;this.number=number;}
-public String getName(){return name;}
-public long getNumber(){return number;}
-public void setName(String name){this.name=name;}
-public void setNumber(long number){this.number=number;}
-public void display(){String msg=String.format("Name: %s\n Number: %s",this.name,this.number);JOptionPane.showMessageDialog(null,msg);}}
