f19a12881c737483bbd18657634b9444f800d61b
John Leonardo <jdleonardo5@icloud.com>
2017-04-23 12:52:09 -0700

    obfuscated

--- Assignments/Chapter 7/7b/PhoneBookEntry.java
+++ Assignments/Chapter 7/7b/PhoneBookEntry.java
@@ -1,45 +1,6 @@
-import javax.swing.JOptionPane;
-
-public class PhoneBookEntry {
-	private String name;
-	private long number;
-	
-	/**
-	 * Constructor for PhoneBookEntry object
-	 * @param name
-	 * @param number
-	 */
-	public PhoneBookEntry(String name, long number) {
-		this.name = name;
-		this.number = number;
-	}
-
-	
-	// --- BEGIN GETTERS/SETTERS --- //
-	public String getName() {
-		return name;
-	}
-
-	public long getNumber() {
-		return number;
-	}
-	
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public void setNumber(long number) {
-		this.number = number;
-	}
-	// --- END GETTERS/SETTERS --- //
-	
-	/**
-	 * Method that displays the current fields in a JOptionPane window
-	 */
-	public void display() {
-		String msg = String.format("Name: %s\n Number: %s", this.name, this.number);
-		JOptionPane.showMessageDialog(null, msg);
-	}
-	
-	
-}
+import javax.swing.JOptionPane;public class PhoneBookEntry{private String name;private long number;public PhoneBookEntry(String name,long number){this.name=name;this.number=number;}
+public String getName(){return name;}
+public long getNumber(){return number;}
+public void setName(String name){this.name=name;}
+public void setNumber(long number){this.number=number;}
+public void display(){String msg=String.format("Name: %s\n Number: %s",this.name,this.number);JOptionPane.showMessageDialog(null,msg);}}
