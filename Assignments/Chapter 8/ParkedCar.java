0e856bb289056102f24370c7dc26024922305d1e
John Leonardo <jdleonardo5@icloud.com>
2017-04-30 18:59:09 -0700

    done with ParkedCar {}

--- Assignments/Chapter 8/ParkedCar.java
+++ Assignments/Chapter 8/ParkedCar.java
@@ -1,1 +1,58 @@
-public class ParkedCar {}
+/**
+ * @author John Leonardo
+ * car's make, model, color, license plate number, & minutes car has been parked
+ */
+public class ParkedCar {
+	
+	// data fields
+	private String make;
+	private String model;
+	private String color;
+	private String licensePlate;
+	private double minutesParked;
+	
+	// constructor with 5 arguments
+	public ParkedCar(String mk, String md, String c, String l, double m) {
+		this.make = mk;
+		this.model = md;
+		this.color = c;
+		this.licensePlate = l;
+		this.minutesParked = m;
+	}
+	
+	// constructor that takes one ParkedCar argument (duplicates it)
+	public ParkedCar(ParkedCar pc) {
+		this.make = pc.make;
+		this.model = pc.model;
+		this.color = pc.color;
+		this.licensePlate = pc.licensePlate;
+		this.minutesParked = pc.minutesParked;
+	}
+	
+	// --- BEGIN GETTERS --- //
+	public double getMinutesParked() {
+		return minutesParked;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+	// --- END GETTERS --- //
+	
+	// we only need one setter, for minutesParked
+	public void setMinutesParked(double minutesParked) {
+		this.minutesParked = minutesParked;
+	}
+}
