967886ad32e4a73eb0fd72c245204c17563728cf
John Leonardo <jdleonardo5@icloud.com>
2017-04-30 19:02:18 -0700

    done with ParkingMeter {}

--- Assignments/Chapter 8/ParkingMeter.java
+++ Assignments/Chapter 8/ParkingMeter.java
@@ -1,1 +1,23 @@
-public class ParkingMeter {}
+/**
+ * @author John Leonardo
+ * one data field, minutes purchased
+ */
+public class ParkingMeter {
+	
+	// data field
+	private double minutesPurchased;
+	
+	public ParkingMeter(double m) {
+		this.minutesPurchased = m;
+	}
+
+	// getter + setter
+	public double getMinutesPurchased() {
+		return minutesPurchased;
+	}
+
+	public void setMinutesPurchased(double minutesPurchased) {
+		this.minutesPurchased = minutesPurchased;
+	}
+	
+}
